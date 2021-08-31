0efa29f261a59f9ee1c46f2bc9c4a470b36c80ee
Lola Rufino <lola.ruf.ar@gmail.com>
2021-08-31 15:41:12 +0200

    Add local strategy and passport config

---
+++ RevelaAndGo/backend/src/config/passport-strategy/localStrategy.js
@@ -0,0 +1,41 @@
+const passport = require('passport');
+const localStrategy = require('passport-local');
+const User = require('../../models/userModel');
+
+passport.use(
+  'signup',
+  new localStrategy.Strategy({
+    usernameField: 'email',
+    passwordField: 'password'
+  },
+  async (email, password, next) => {
+    try {
+      const user = await User.create({ email, password });
+      return next(null, user);
+    } catch (error) {
+      return next(error);
+    }
+  })
+);
+
+passport.use(
+  'login',
+  new localStrategy.Strategy({
+    usernameField: 'email',
+    passwordField: 'password'
+  },
+  async (email, password, next) => {
+    try {
+      const user = await User.findOne({ email });
+      if (!user) {
+        return next(null, false, { message: 'User not found' });
+      }
+      if (!user.isValidPassword(password)) {
+        return next(null, false, { message: 'Wrong password' });
+      }
+      return next(null, user, { message: 'Logged in successfully' });
+    } catch (error) {
+      return next(null, false);
+    }
+  })
+);
