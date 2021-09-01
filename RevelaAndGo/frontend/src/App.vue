e02d1b8e3417a02bff217aef215937e52f966599
Lola Rufino <lola.ruf.ar@gmail.com>
2021-09-01 19:37:03 +0200

    Configure sonarqube properties

--- RevelaAndGo/frontend/src/App.vue
+++ RevelaAndGo/frontend/src/App.vue
@@ -6,19 +6,10 @@
       <router-link to="/Cart">Cart</router-link>
     </div>
     <router-view />
-  </div>
 </template>
 
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #af4c86;
-}
-
 #nav {
   padding: 30px;
 
