aa85217a8b5abdc76f5c1737dbfdadd61c3c9c8f
Lola Rufino <lola.ruf.ar@gmail.com>
2021-09-02 13:32:19 +0200

    Add footer

--- RevelaAndGo/frontend/src/App.vue
+++ RevelaAndGo/frontend/src/App.vue
@@ -5,26 +5,27 @@
       <router-link to="/Profile">Profile</router-link> |
       <router-link to="/Cart">Cart</router-link>
     </div>
-    <button class="button">This is a button</button>
-    <div class="div">This a container</div>
     <router-view />
+    <Footer />
   </div>
 </template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+import Footer from "./components/Footer.vue";
+
+export default defineComponent({
+  components: {
+    Footer,
+  },
+});
+</script>
 
 
 <style lang="scss">
 @import "./styles/mixins";
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #682f5b;
-
-    &.router-link-exact-active {
-      color: #a366a0;
-    }
-  }
+* {
+  margin: 0;
 }
 .button {
   @include button;
---
+++ RevelaAndGo/frontend/src/components/Footer.vue
@@ -0,0 +1,29 @@
+<template>
+  <footer class="footer">
+    <small class="footer__copyright">© Revela & Go 2021</small>
+    <img
+      class="footer__image"
+      src="https://i.ibb.co/ZTbzqVh/bottomroll.png"
+      alt="Footer image of a film roll"
+    />
+  </footer>
+</template>
+
+<style lang="scss">
+@import "../styles/variables";
+
+.footer {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+
+.footer__copyright {
+  font-family: $font;
+  font-weight: 900;
+}
+
+.footer__image {
+  width: 100%;
+}
+</style>
