2254f48b0dd623bf40a579110d95b9b5774f6298
Lola Rufino <lola.ruf.ar@gmail.com>
2021-09-05 17:49:58 +0200

    Delete not needed library v wave

--- RevelaAndGo/frontend/src/main.ts
+++ RevelaAndGo/frontend/src/main.ts
@@ -2,13 +2,5 @@ import { createApp } from 'vue';
 import App from './App.vue';
 import router from './router';
 import store from './store';
-import VWave from 'v-wave';
 
-createApp(App).use(store).use(VWave, {
-    color: 'red',
-    easing: 'ease-out',
-    duration: 0.4,
-    initialOpacity: 0.2,
-    finalOpacity: 0.1,
-    cancellationPeriod: 95,
-  }).use(router).mount('#app');
+createApp(App).use(store).use(router).mount('#app');
