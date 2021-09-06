77aace4d8f1916894d110ce5e142f8f5d2e32870
Lola Rufino <lola.ruf.ar@gmail.com>
2021-09-06 10:37:47 +0200

    Change direction in auth.ts routes

--- RevelaAndGo/frontend/src/auth/auth.ts
+++ RevelaAndGo/frontend/src/auth/auth.ts
@@ -1,15 +1,13 @@
 import axios from "axios";
 
-const ENDPOINT_PATH = "http://localhost:5000/api/";
-
 export default {
   register(email: any, password: any) {
     console.log(email, password);
     const user = { email, password };
-    return axios.post(ENDPOINT_PATH + "register", user);
+    return axios.post("http://localhost:5000/api/register", user);
   },
   login(email: any, password: any) {
     const user = { email, password };
-    return axios.post(ENDPOINT_PATH + "login", user);
+    return axios.post("http://localhost:5000/api/login", user);
   }
 };
