2038925e69556006e038e23f2004a9d29e939929
Lola Rufino <lola.ruf.ar@gmail.com>
2021-09-06 09:44:40 +0200

    Fix test of Login view

--- RevelaAndGo/frontend/src/views/Login.vue
+++ RevelaAndGo/frontend/src/views/Login.vue
@@ -31,12 +31,7 @@
       />
     </div>
     <p v-if="error">Has introducido mal el email o la contraseña.</p>
-    <input
-      type="submit"
-      value="Login"
-      class="login__button"
-      data-test="login"
-    />
+    <input type="submit" value="Login" class="login__button" />
     <small>
       ¿Sin cuenta?
       <router-link class="login__register" to="/register"
