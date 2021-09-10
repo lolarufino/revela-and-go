4c9b8854a9fc28568f0f5760c7615c4c4538c825
Lola Rufino <lola.ruf.ar@gmail.com>
2021-09-10 11:27:52 +0200

    Add environment variables for security reasons

--- RevelaAndGo/frontend/src/auth/auth.ts
+++ RevelaAndGo/frontend/src/auth/auth.ts
@@ -2,15 +2,15 @@ import axios from "axios";
 
 export default {
   async register(email: string, password: string) {
-    const userCart = await axios.post('http://localhost:5000/api/cart');
+    const userCart = await axios.post(`${process.env.VUE_APP_API_URL}/cart`);
     const user = { email, password };
     const userWithCart = { ...user, cart: userCart.data._id };
-    const {data} = await axios.post("http://localhost:5000/api/register", userWithCart);
+    const {data} = await axios.post(process.env.VUE_APP_REGISTER_URL, userWithCart);
     return data;
   },
   async login(email: string, password: string) {
     const user = { email, password };
-    const {data} = await axios.post("http://localhost:5000/api/login", user);
+    const {data} = await axios.post(process.env.VUE_APP_LOGIN_URL, user);
     return data;
   }
 };
