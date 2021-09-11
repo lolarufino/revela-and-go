f797a3a571ed989b7831d25984c2fdc5a0d57579
Lola Rufino <lola.ruf.ar@gmail.com>
2021-09-11 22:21:56 +0200

    Add testing for searchlab component

--- RevelaAndGo/frontend/tests/unit/SearchLab.spec.ts
+++ RevelaAndGo/frontend/tests/unit/SearchLab.spec.ts
@@ -1,10 +1,26 @@
 import { mount } from '@vue/test-utils';
+import router from '../../src/router';
+import state from '../mockedState';
 import SearchLab from '../../src/views/SearchLab.vue';
 
 describe('Given a SearchLab component', () => {
     describe('When is rendered', () => {
         test('Should render a found labs text', () => {
-            const wrapper = mount(SearchLab)
+            const wrapper = mount(SearchLab, {
+                global: {
+                  plugins: [router],
+                  mocks: {
+                    $store: {
+                      state,
+                      actions: {
+                        fetchLabsFromApi: jest.fn(),
+                      },
+                      dispatch: jest.fn(),
+                      commit: jest.fn(),
+                    },
+                  },
+                },
+              })
           
             expect(wrapper.html()).toContain('<p class="labslist__header" data-test="foundlabs"> Estos son los laboratorios que corresponden a tu búsqueda:</p>')
           })
