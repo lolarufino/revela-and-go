9f6bcd0ea9d49fe0d332b79566bdf801fe7fd432
Lola Rufino <lola.ruf.ar@gmail.com>
2021-09-11 16:22:42 +0200

    Add mocked state for tests

---
+++ RevelaAndGo/frontend/tests/mockedState.ts
@@ -0,0 +1,14 @@
+const mockedState = {
+    labs: [],
+    user: '',
+    userId: '',
+    cartId: '',
+    serviceId: '',
+    token: '',
+    refreshToken: '',
+    lab: {},
+    finalService: [],
+    service: {},
+    price: 0,
+    isLoggedIn: true
+  }
