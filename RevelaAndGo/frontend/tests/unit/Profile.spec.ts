10395d02a8d5a2d1230cf88f39c5a0b457ea4cd9
Lola Rufino <lola.ruf.ar@gmail.com>
2021-09-12 11:54:12 +0200

    Fix profile testing error

--- RevelaAndGo/frontend/tests/unit/Profile.spec.ts
+++ RevelaAndGo/frontend/tests/unit/Profile.spec.ts
@@ -1,14 +1,30 @@
 import { mount } from '@vue/test-utils';
+import router from '../../src/router';
+import state from '../mockedState';
 import Profile from '../../src/views/Profile.vue';
 
 describe('Given a Profile component', () => {
     describe('When is rendered', () => {
         test('Should render a nombre text', () => {
-            const wrapper = mount(Profile)
+            const wrapper = mount(Profile, {
+                global: {
+                  plugins: [router],
+                  mocks: {
+                    $store: {
+                      state,
+                      actions: {
+                        fetchUserLoggedFromApi: jest.fn(),
+                      },
+                      dispatch: jest.fn(),
+                      commit: jest.fn(),
+                    },
+                  },
+                },
+            })
           
-            const name = wrapper.get('[data-test="userName"]')
+            const name = wrapper.get('[data-test="email"]')
           
-            expect(name.text()).toBe('Nombre:')
+            expect(name.text()).toBe('E-mail:')
           })
     })
 })
